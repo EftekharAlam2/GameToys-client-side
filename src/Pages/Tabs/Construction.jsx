84a09cfb3b6725960de4eb9af31b06fff1e19353
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-20 21:56:28 +0600

    3 tab added

--- src/Pages/Tabs/Construction.jsx
+++ src/Pages/Tabs/Construction.jsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Construction = () => {
+  const [toysData, setToysData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/toys", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const constructionToys = data.filter(
+          (toy) => toy.category === "Construction Sets"
+        );
+        setToysData(constructionToys);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Constructions</h1>
+    <div
+      className="max-w-7xl my-10 mx-auto"
+      data-aos="flip-left"
+      data-aos-delay="800"
+      data-aos-anchor=".example-selector"
+    >
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {toysData.map((toy) => (
+          <div key={toy._id} className="bg-white rounded-lg shadow-md">
+            <img
+              src={toy.photoURL}
+              alt="Photo Name"
+              className="h-96 w-full object-cover rounded-t-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-2">{toy.toyName}</h3>
+              <p className="text-gray-700 mb-2">Price: ${toy.price}</p>
+              <div className="flex items-center mb-2">
+                <span className="text-yellow-500 mr-1">
+                  Rating: {toy.rating}
+                </span>
+              </div>
+              <Link to={`/toydetails/${toy._id}`}>
+                <button className="btn btn-outline btn-warning text-white py-2 px-4 rounded-lg">
+                  View Details
+                </button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
