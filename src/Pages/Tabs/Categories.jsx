1b38a4a440004118ab56396fa86cd4bb0151c5de
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-08-12 14:50:13 +0600

    Categories page remove

--- src/Pages/Tabs/Categories.jsx
+++ src/Pages/Tabs/Categories.jsx
@@ -1,13 +1,13 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import Action from "./Action";
-import Plush from "./Plush";
-import Construction from "./Construction";
+// import Action from "./Action";
+// import Plush from "./Plush";
+// import Construction from "./Construction";
 
 const Categories = () => {
   return (
     <Tabs>
-      <h2 className="text-3xl text-center font-bold mb-2 mt-7">
+      {/* <h2 className="text-3xl text-center font-bold mb-2 mt-7">
         Shop by Category
       </h2>
       <TabList>
@@ -24,7 +24,7 @@ const Categories = () => {
       </TabPanel>
       <TabPanel>
         <Construction></Construction>
-      </TabPanel>
+      </TabPanel> */}
     </Tabs>
   );
 };
